da7a0854061ba3c0dd4beb52683840f19ea9d3cb
Renee <contact@heyrenee.ca>
2013-03-21 15:47:32 -0400

    updating views

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,30 +1,38 @@
 class PhotosController < ApplicationController
   before_filter :authenticate_user!
+  
+  respond_to :html, :json 
+  
+  def index
+    respond_with @photos = Photo.order("name").page(params[:page]).per(5)
+  end
+  
+  def show
+    respond_with @photo = current_user.photos.find_by_id( params[:id] )
+  end
+  
   def new
-    @photo = current_user.photos.new
-  end
-
-  def create
-    @photo = current_user.photos.create(params[:photo])
-  end
-
-  def update
-    @photo = current_user.photos.update
+    respond_with @photo = current_user.photos.new( params[:photo] )
   end
 
   def edit
-    @photo = current_user.photos.edit
+    respond_with @photo = current_user.photos.find_by_id( params[:id] )
+  end
+
+
+  def create
+    @photo = current_user.photos.create(params[:photo])
+    redirect_to photo_path(@photo)
+  end
+
+  def update
+    respond_with @photo = current_user.photos.update( params[:id], params[:photo] )
+    redirect_to edit_photo_path(@photo)
   end
 
   def destroy
-    @photo current_user.photos.delete
+    respond_withs @photo = current_user.photos.find_by_id(params[:id]).destroy 
+    redirect_to photos_path
   end
 
-  def index
-    @photo = current_user.photos.all
-  end
-
-  def show
-    @photo = current_user.photos.find_by_id params[:id]
-  end
 end
